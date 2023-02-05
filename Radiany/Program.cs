48756190ee0b1ce4fcdcfcdb318ef92d2b4f831e
Jacek Kupiec <jacek.kupiec@o2.pl>
2023-02-05 17:18:30 +0100

    funkcje + teksty

---
+++ Radiany/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Radiany
+{
+    internal class Program
+    {
+        static double StopnieNaRadiany(double stopnie)
+        {
+            return stopnie * double.Pi / 180;
+        }
+
+        static double RadianyNaStopnie(double radiany)
+        {
+            return radiany * 180 / double.Pi;
+        }
+
+        // rozwiązuje równanie ax^2 + bx + c = 0
+        static (double, double) RownanieKwadratowe(double a, double b, double c)
+        {
+            var delta = b * b - 4 * a * c;
+
+            if (delta >= 0)
+            {
+                var delta_sqrt = Math.Sqrt(delta);
+                var x1 = (-b - delta_sqrt) / (2 * a);
+                var x2 = (-b + delta_sqrt) / (2 * a);
+
+                return (x1, x2);
+            }
+            else
+            {
+                return (double.NaN, double.NaN);
+            }
+
+            // to się nie wykona, martwy kod
+            Console.WriteLine("trele morele");
+        }
+
+        static void Main(string[] args)
+        {
+            var stopnie = 90;
+
+            // zamieniamy na radiany
+            var radiany = StopnieNaRadiany(stopnie);
+
+            Console.WriteLine(radiany);
+
+            var zpowrotem90 = RadianyNaStopnie(radiany);
+            Console.WriteLine(zpowrotem90);
+
+            // x^2 - 3x + 2 = 0 
+            var (r, mateusz) = RownanieKwadratowe(1, -3, 2);
+
+            Console.WriteLine("x1 = {0}, x2 = {1}", r, mateusz);
+        }
+    }
+}
---
+++ Teksty/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Teksty
+{
+    internal class Program
+    {
+        /* 
+         * byte (0 - 255), sbyte (-128, 127)
+         * short int, short uint 2 bajty
+         * int, uint 4 bajty
+         * long, ulong 8 bajtów
+         * float (4 bajty), double (8 bajtów)
+         * decimal 12 bajtów
+         * char 2 bajty przechowuje kody znaków
+         * string char[]
+         */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ABBA"); // { 'A', 'B', 'B', 'A', 0 }
+            string tekst1 = "Ala ma kota"; // "A", 'A'
+            char[] tekst2 = new char[] { 'A', 'J' }; // "AJ" => {'A', 'J', 0}
+
+            var text_result = tekst1 + "qwerty"; // => "Alamakotaqwerty"
+
+            foreach (char c in text_result) {
+                Console.WriteLine(c);
+            }
+
+            var inny_tekst = "1, 2, 3, 4, 5, 6";
+            var tabelka = new int[6];
+            var podzielony_tekst = inny_tekst.Split(", ");
+
+            int idx = 0;
+
+            foreach (string tekst_po_podzieleniu in podzielony_tekst) 
+            {
+                tabelka[idx] = int.Parse(tekst_po_podzieleniu);
+                idx = idx + 1;
+            }
+
+            for (int i = 0; i < podzielony_tekst.Length; i = i + 1) 
+            {
+                tabelka[i] = int.Parse(podzielony_tekst[i]);
+            }
+        }
+    }
+}
